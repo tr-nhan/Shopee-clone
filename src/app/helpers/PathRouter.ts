f3c6477e6f75fdd98a4eadf2aafcf414940ad3bd
tr-nhan <letrungnhan021704@gmail.com>
2023-02-28 03:12:13 +0700

    config .env

--- src/app/helpers/PathRouter.ts
+++ src/app/helpers/PathRouter.ts
@@ -1,15 +1,17 @@
-import {Application} from "express";
+import { Application } from "express";
 import SitesController from "../sites/SitesController.js";
+import UserController from "../user/UserController.js";
 
 
 export default class PathRouter {
-    private app: Application;
+  private app: Application;
 
-    public constructor(app: Application) {
-        this.app = app;
-    }
+  public constructor(app: Application) {
+    this.app = app;
+  }
 
-    public route(): void {
-        this.app.use("/", SitesController.requestMapping());
-    }
+  public route(): void {
+    this.app.use("/user", UserController.requestMapping());
+    this.app.use("/", SitesController.requestMapping());
+  }
 }
