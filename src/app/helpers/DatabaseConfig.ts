c8484d09edde54fce44d405833b7a3e65851e6fe
tr-nhan <letrungnhan021704@gmail.com>
2023-03-09 20:00:52 +0700

    Delete default database storage

--- src/app/helpers/DatabaseConfig.ts
+++ src/app/helpers/DatabaseConfig.ts
@@ -1,12 +1,11 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-
+import * as dotenv from "dotenv";
 export default class DatabaseConfig {
   public static getInstance(): Sequelize {
     dotenv.config();
     return new Sequelize({
       dialect: "sqlite",
-      storage: process.env.DB_HOST || "./database.sqlite",
+      storage: process.env.DB_HOST,
       pool: {
         max: 9,
         min: 0,
